e59ae942bf1b78f0920f8020f506b352a398d10e
chiara68 <n.chiara@toknar.io>
2025-05-25 03:47:52 +0100

    fix after 6

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 //import './index.css'
 //import App from './App.tsx'
 import { Amplify } from 'aws-amplify'
-import config from '../amplify_outputs.json'
+import config from './amplify_outputs.json'
 import App from './App'
 Amplify.configure(config);
 // Create a typed config that matches Amplify expectations
