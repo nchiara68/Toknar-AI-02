c2658eb1ff08e6ea105403a7919b9af2f510cdbe
chiara68 <n.chiara@toknar.io>
2025-06-04 00:06:23 +0400

    Force storage backend to trigger build

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,14 +1,10 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'myFileManagerBucket',
+  name: 'myFileManagerBucket-fix', // <- change the name slightly
   isDefault: true,
   access: (allow) => ({
-    'folder-a/*': [
-      allow.authenticated.to(['read', 'write', 'delete'])
-    ],
-    'folder-b/*': [
-      allow.authenticated.to(['read', 'write', 'delete'])
-    ],
+    'folder-a/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    'folder-b/*': [allow.authenticated.to(['read', 'write', 'delete'])],
   }),
 });
